0e9a7dd3feba38c56ff0293a2480f71a515e4f70
roshh4 <roshinidharmaraj@gmail.com>
2025-06-07 00:21:19 +0530

    initial

---
+++ move0toEnd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> a = {0,0,0,0,1,0,2,0,3,0,4};
+    // int x1 = 0, x2 = 0;
+    // for(int i = 0; i < a.size(); i++)
+    // {
+
+    // }
+    // cout << x1 ^ x2;
+    
+}
