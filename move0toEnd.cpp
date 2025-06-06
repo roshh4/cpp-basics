d015a017ff81b166c854e7acdb9a47d17ed8c0d6
roshh4 <roshinidharmaraj@gmail.com>
2025-06-07 00:53:47 +0530

    moving 0 to end

--- move0toEnd.cpp
+++ move0toEnd.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main()
 {
     vector<int> a = {0,0,0,0,1,0,2,0,3,0,4};
-    // int x1 = 0, x2 = 0;
-    // for(int i = 0; i < a.size(); i++)
-    // {
-
-    // }
-    // cout << x1 ^ x2;
-    
+    int n = a.size();
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == 0)
+        {
+            rotate(a.begin() + i, a.begin() + i + 1, a.end());
+            n--;
+        }
+    }
+    for(int i : a)
+    {
+        cout << i << " ";
+    }
 }
