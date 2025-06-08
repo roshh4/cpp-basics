0b185ccf1b9ea423a2e6f4c3d9519a5e94a2fa4d
roshh4 <roshinidharmaraj@gmail.com>
2025-06-08 22:14:25 +0530

    anagarm v3

--- strings/anagram.cpp
+++ strings/anagram.cpp
@@ -23,19 +23,45 @@ using namespace std;
 //     }
 // }
 
+
+//v2, ascii indexed array
+// int main()
+// {
+//     string s = "rrsshh";
+//     string t = "rshrhh";
+//     int x1[128] = {0};
+//     int x2[128] = {0};
+//     for(int i = 0; i < s.length(); i++)
+//     {
+//         x1[s[i]]++;
+//         x2[t[i]]++;
+//     }
+//     bool flag = true;
+//     int n = sizeof(x1)/sizeof(x1[0]);
+//     for(int i = 0; i < n; i++)
+//     {
+//         if(x1[i] != x2[i])
+//         {
+//             flag = false;
+//             break;
+//         }
+//     }
+//     cout << flag;
+// }
+
+//v3: array of size 26
 int main()
 {
-    string s = "rrsshh";
-    string t = "rshrhh";
-    int x1[128] = {0};
-    int x2[128] = {0};
+    string s = "anagram";
+    string t = "nagrram";
+    int x1[26] = {0}, x2[26] = {0};
     for(int i = 0; i < s.length(); i++)
     {
-        x1[s[i]]++;
-        x2[t[i]]++;
+        x1[s[i] - 'a']++;
+        x2[t[i] - 'a']++;
     }
-    bool flag = true;
     int n = sizeof(x1)/sizeof(x1[0]);
+    bool flag = true;
     for(int i = 0; i < n; i++)
     {
         if(x1[i] != x2[i])
