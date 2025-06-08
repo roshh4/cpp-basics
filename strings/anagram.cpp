26ddd254df6452d1295db5c39e70b5c5e4856b93
roshh4 <roshinidharmaraj@gmail.com>
2025-06-08 20:27:17 +0530

    anagarm v2

---
+++ strings/anagram.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// sol 1
+// int main()
+// {
+//     string s = "abb";
+//     string t = "aac";
+//     int x1 = 0, x2 = 0;
+//     for(int i = 0; i < s.length(); i++)
+//     {
+//         x1 += s[i];
+//         x2 += t[i];
+//     }
+//     if(x1 == x2)
+//     {
+//         cout << "valid anagram";
+//     }
+//     else{
+//         cout << "nahhh bitch";
+//     }
+// }
+
+int main()
+{
+    string s = "rrsshh";
+    string t = "rshrhh";
+    int x1[128] = {0};
+    int x2[128] = {0};
+    for(int i = 0; i < s.length(); i++)
+    {
+        x1[s[i]]++;
+        x2[t[i]]++;
+    }
+    bool flag = true;
+    int n = sizeof(x1)/sizeof(x1[0]);
+    for(int i = 0; i < n; i++)
+    {
+        if(x1[i] != x2[i])
+        {
+            flag = false;
+            break;
+        }
+    }
+    cout << flag;
+}
